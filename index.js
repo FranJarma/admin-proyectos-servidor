00627db4b7d5823a7276997ac0e86bd19f6e698d
Francisco Jarma <pancho24.1997@gmail.com>
2020-09-17 14:56:21 -0300

    cambio en index.js

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ conectarBaseDeDatos();
 //habilitamos cors
 app.use(cors());
 //para leer datos que ingrese el usuario
+
 app.use(express.json({ extended: true}));
+// Website you wish to allow to connect
+res.setHeader('Access-Control-Allow-Origin', 'https://admin-proyectos-mern.herokuapp.com/api/auth');
+
+// Request methods you wish to allow
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+// Request headers you wish to allow
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+// Set to true if you need the website to include cookies in the requests sent
+// to the API (e.g. in case you use sessions)
+res.setHeader('Access-Control-Allow-Credentials', true);
 
 //puerto de APP
 const port = process.env.port || 4000;
