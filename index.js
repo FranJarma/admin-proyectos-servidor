6ba259302728caf55bf5eda463ce0fe1aef8ba37
Francisco Jarma <pancho24.1997@gmail.com>
2020-09-17 15:17:15 -0300

    cambio en index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json({ extended: true}));
 
 
 //puerto de APP
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 // Importamos rutas
 app.use('/api/usuarios', require('./routes/usuarios.js'));
