8817ddb4c07d316fbd624b8ea1da54cb52cfa74e
Francisco Jarma <pancho24.1997@gmail.com>
2020-09-17 15:05:49 -0300

    cambio en index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ conectarBaseDeDatos();
 //habilitamos cors
 app.use(cors());
 //para leer datos que ingrese el usuario
-app.use(cors({origin:true,credentials: true}));
+app.use(cors({origin:'https://administrador-proyectos-mern.netlify.app',credentials: true}));
 app.use(express.json({ extended: true}));
 
 
